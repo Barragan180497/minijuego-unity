4676202dad0449df309df1878e1fe361bb149591
JhoanRc <jreyes549@unab.edu.co>
2019-03-09 16:11:27 -0500

    Se añade menu de inicio
    Se introdujeron 2 escenas, prefabs y demás para la gestión del menú.

---
+++ MinJuego_Espada/Assets/Scripts/botonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class botonScript : MonoBehaviour
+{
+
+    
+
+    public void Escene(string nombreEscena){
+		SceneManager.LoadScene(nombreEscena);
+	}
+
+	public void QuitGame(){
+
+		Application.Quit();
+	}
+}
